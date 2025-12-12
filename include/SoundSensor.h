923080bca4027f301b1bf427ce60055e00477b33
UFUKtr08 <ufukyzmain@gmail.com>
2025-12-12 03:33:59 +0300

    Fix:Camera.h and SoundSensor.h were not have togglePower funct. and this patch fixes it

--- include/SoundSensor.h
+++ include/SoundSensor.h
@@ -2,6 +2,7 @@
 #define SOUNDSENSOR_H
 
 #include "Device.h"
+#include <iostream>
 
 class SecuritySystem;
 
@@ -9,6 +10,12 @@ class SoundSensor : public Device {
 public:
     SoundSensor(int id, string n) : Device(id, n) {}
     ~SoundSensor() {}
+
+    // EKSİK OLAN PARÇA BU:
+    void togglePower() {
+        isPowered = !isPowered;
+        cout << getName() << (isPowered ? " is now ON." : " is now OFF.") << endl;
+    }
 
     Device* clone() const { return new SoundSensor(*this); }
 
