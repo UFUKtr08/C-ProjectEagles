7638b2a71816453b64c5c23564c0c7c4886e91d7
BatuhanK7 <210201011@ostimteknik.edu.tr>
2025-12-15 00:39:37 +0300

    Mod onay mesaji eklendi ve About bolumu guncellendi

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,17 @@
 
 using namespace std;
 
+// YARDIMCI FONKSİYON: Sade Liste
+void printSimpleList(vector<Device*>& devices) {
+    cout << "\n--- EV DURUMU ---" << endl;
+    for (size_t i = 0; i < devices.size(); ++i) {
+        string status = devices[i]->getPowerStatus() ? "ON" : "OFF";
+        cout << "[" << devices[i]->getID() << "] " 
+             << devices[i]->getName() 
+             << " [" << status << "]" << endl;
+    }
+}
+
 int main() {
   Logger::getInstance()->log("--- SYSTEM BOOT SEQUENCE INITIATED ---", "SYSTEM");
 
@@ -37,20 +48,23 @@ int main() {
   deviceMgr.addDevice("TV", "Smart TV");
   deviceMgr.addDevice("Curtain", "Balcony Curtain");
   deviceMgr.addDevice("Light", "Toilet Light");      
-  deviceMgr.addDevice("SmartFan", "Bathroom Fan");   
+  deviceMgr.addDevice("SmartFan", "Bathroom Fan");
+  deviceMgr.addDevice("SmartFaucet", "Kitchen Faucet");   
   deviceMgr.addDevice("Stove", "Kitchen Stove");     
 
   bool isRunning = true;
   while (isRunning) {
     menu.displayOptions();
+    
+    // Su Taşkını seçeneği kaldırıldı
     cout << "[11] Wait / Pass Time (Trigger Automations)" << endl;
 
+    // Seçenek sınırını tekrar 11 yaptık
     int choice = input.getIntInput(1, 11, "\n>> Select Option: ");
     vector<Device *> &currentDevices = deviceMgr.getDevicesRef();
 
     switch (choice) {
     case 1: 
-        // Zaten listeyi gosteriyor
         deviceMgr.listDevices(); 
         input.waitPressEnter(); 
         break;
@@ -69,67 +83,72 @@ int main() {
     }
 
     case 3: // Device Silme
-        // Silmeden once de neyi silecegimizi gormek isteriz
-        deviceMgr.listDevices(); 
+        printSimpleList(currentDevices); 
         deviceMgr.removeDevice(input.getIntInput(0, 1000, "Enter Device ID to Remove: ")); 
         input.waitPressEnter(); 
         break;
 
-    case 4: // POWER ON (DUZELTİLDİ)
+    case 4: // POWER ON
     {
-        // 1. ADIM: Önce listeyi ekrana basıyoruz (Prototipindeki gibi)
-        deviceMgr.listDevices();
-        
-        // 2. ADIM: Sonra ID istiyoruz
-        int id = input.getIntInput(0, 1000, "Enter Device ID to turn ON: ");
-        Device* d = deviceMgr.getDevice(id);
-        
-        if(d) {
-            if (!d->getPowerStatus()) {
-                d->togglePower();
-                // Otomasyon kontrolü
-                if(d->getType() == "Light") deviceMgr.notifyLightSwitch(true, d->getName());
-            } else {
-                cout << ">> Info: Device is already ON." << endl;
-            }
+        printSimpleList(currentDevices);
+        cout << "Islem yapilacak Cihaz ID (veya 0 Hepsini Ac): ";
+        int id;
+        if(!(cin >> id)) { cin.clear(); cin.ignore(1000, '\n'); id = -1; }
+
+        if (id == 0) {
+            for(size_t i=0; i<currentDevices.size(); i++) 
+                if(!currentDevices[i]->getPowerStatus()) currentDevices[i]->togglePower();
+            cout << ">> Tum cihazlar acildi." << endl;
         } else {
-            cout << "!! Error: Device not found." << endl;
+            Device* d = deviceMgr.getDevice(id);
+            if(d) {
+                if (!d->getPowerStatus()) {
+                    d->togglePower();
+                    if(d->getType() == "Light") deviceMgr.notifyLightSwitch(true, d->getName());
+                } else cout << ">> Info: Device is already ON." << endl;
+            } else cout << "!! Error: Device not found." << endl;
         }
         input.waitPressEnter(); 
         break;
     }
 
-    case 5: // POWER OFF (DUZELTİLDİ)
+    case 5: // POWER OFF
     {
-        // Burada da aynısını yapıyoruz
-        deviceMgr.listDevices();
-        
-        int id = input.getIntInput(0, 1000, "Enter Device ID to turn OFF: ");
-        Device* d = deviceMgr.getDevice(id);
-        
-        if(d) {
-            if (d->getPowerStatus()) {
-                d->togglePower();
-                // Otomasyon kontrolü
-                if(d->getType() == "Light") deviceMgr.notifyLightSwitch(false, d->getName());
-            } else {
-                cout << ">> Info: Device is already OFF." << endl;
-            }
+        printSimpleList(currentDevices);
+        cout << "Islem yapilacak Cihaz ID (veya 0 Hepsini Kapat): ";
+        int id;
+        if(!(cin >> id)) { cin.clear(); cin.ignore(1000, '\n'); id = -1; }
+
+        if (id == 0) {
+            for(size_t i=0; i<currentDevices.size(); i++) 
+                if(currentDevices[i]->getPowerStatus()) currentDevices[i]->togglePower();
+            cout << ">> Tum cihazlar kapatildi." << endl;
         } else {
-            cout << "!! Error: Device not found." << endl;
+            Device* d = deviceMgr.getDevice(id);
+            if(d) {
+                if (d->getPowerStatus()) {
+                    d->togglePower();
+                    if(d->getType() == "Light") deviceMgr.notifyLightSwitch(false, d->getName());
+                } else cout << ">> Info: Device is already OFF." << endl;
+            } else cout << "!! Error: Device not found." << endl;
         }
         input.waitPressEnter(); 
         break;
     }
 
-    case 6: // Change Mode
+    case 6: // Change Mode (ONAY MESAJLI)
     {
         cout << "[1] Normal [2] Party [3] Cinema" << endl;
         int m = input.getIntInput(1, 3, "Select Mode: ");
+        
         if(m==1) modeMgr.setMode(new NormalMode(), currentDevices);
         else if(m==2) modeMgr.setMode(new PartyMode(), currentDevices);
         else if(m==3) modeMgr.setMode(new CinemaMode(), currentDevices);
-        input.waitPressEnter(); break;
+        
+        cout << "\n>> MOD DEGISTIRME BASARILI: " << modeMgr.getCurrentModeName() << " Modu Aktif Edildi." << endl;
+        
+        input.waitPressEnter(); 
+        break;
     }
 
     case 7: 
@@ -161,7 +180,23 @@ int main() {
         input.waitPressEnter(); break;
     }
 
-    case 9: cout << "MSH v3.0 - C++98 Edition" << endl; input.waitPressEnter(); break;
+    case 9: // ABOUT MENÜSÜ (İsim Listesi)
+    {
+        cout << "\n========================================" << endl;
+        cout << "Proje Adi: My Sweet Home (MSH) v3.0" << endl;
+        cout << "========================================" << endl;
+        cout << "" << endl;
+        cout << "Merve Selin Umit---220201028" << endl;
+        cout << "Ufuk Yilmaz---210201024" << endl;
+        cout << "Batuhan Kayalica---210201011" << endl;
+        cout << "Baran Acur---210201036" << endl;
+        cout << "Fatih Ozcan---210201028" << endl;
+        cout << "Ridvan Serbes---210201038" << endl;
+        cout << "========================================" << endl;
+        input.waitPressEnter(); 
+        break;
+    }
+    
     case 10: isRunning = false; cout << "Shutting down..." << endl; break;
     
     case 11: 
